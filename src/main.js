23b707cd39322ee86c47460ef6dae05da7aa70b4
wanghuai <2515323344@qq.com>
2019-11-24 14:15:34 +0800

    2 day

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import './css/global.css'
+import axios from 'axios'
+Vue.prototype.$http = axios
+axios.defaults.baseURL='http://127.0.0.1:8888/api/private/v1/'
 
 Vue.config.productionTip = false
 
